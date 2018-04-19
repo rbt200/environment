1f69a488700a821503f76866a9338a501abdc00c
rbt200 <myesoap@mail.ru>
2018-04-19 21:13:37 +0300

    before deploy

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,16 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res){
+    // Hard coding for simplicity. Pretend this a real database
+    res.json([
+        {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+        {"id": 2,"firstName":"John","lastName":"Simons","email":"john@gmail.com"},
+        {"id": 3,"firstName":"Jane","lastName":"Janaton","email":"jane@gmail.com"},
+        {"id": 4,"firstName":"Liz","lastName":"Simpson","email":"liz@gmail.com"}
+    ]);
+});
+
 app.listen(port, function(err) {
     if (err) {
         console.log(err);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import './index.css';
-import numeral from 'numeral';
+import {getUsers} from './api/userApi';
 
-const courseValue = numeral(1000).format('$0,0.00');
+// Ppopulate table of users via API call
+getUsers().then(result => {
+    let usersBody = "";
 
-// debugger;
+    result.forEach(user => {
+        usersBody += `<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.LastName}</td>
+        <td>${user.email}</td>
+        </tr>`
+    });
+    global.document.getElementById('users').innerHTML = usersBody;
+});
+// import './index.css';
+// import numeral from 'numeral';
 
-/* eslint-disable no-console */
-console.log(`I would pay ${courseValue} for this ausom course!`); 
+// const courseValue = numeral(1000).format('$0,0.00');
+
+// // debugger;
+
+// /* eslint-disable no-console */
+// console.log(`I would pay ${courseValue} for this ausom course!`); 
